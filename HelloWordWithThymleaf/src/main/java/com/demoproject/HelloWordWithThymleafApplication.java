c25548116986d15e21c084607bd1087fce63c4de
deepak-8bit <96579345+deepak-8bit@users.noreply.github.com>
2022-01-17 00:55:10 +0530

    Add files via upload

---
+++ HelloWordWithThymleaf/src/main/java/com/demoproject/HelloWordWithThymleafApplication.java
@@ -0,0 +1,13 @@
+package com.demoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWordWithThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWordWithThymleafApplication.class, args);
+	}
+
+}
